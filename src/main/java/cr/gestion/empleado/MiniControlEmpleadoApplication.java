f55530763482e6e35b352018a84fbe4dfaf4fb1e
Cesar <cesartetsu@gmail.com>
2022-04-23 21:34:27 -0500

    primer commit

---
+++ src/main/java/cr/gestion/empleado/MiniControlEmpleadoApplication.java
@@ -0,0 +1,13 @@
+package cr.gestion.empleado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniControlEmpleadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniControlEmpleadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cr/gestion/empleado/MiniControlEmpleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.gestion.empleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniControlEmpleadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
